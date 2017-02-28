e20bedda2315c2dab9621f5e408cad86db8ad721
Nicholas Hoyt <nick@webui.com>
2017-02-27 20:36:56 -0600

    Initial commit

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SRCDIR=/Users/nhoyt/Documents/Notes/Reference
+
+cp -fp $SRCDIR/Git-Basics.txt     ./basics.md
+cp -fp $SRCDIR/Git-Commands.txt   ./commands.md
+cp -fp $SRCDIR/Git-Submodules.txt ./submodules.md
+cp -fp $SRCDIR/Git-Subtrees.txt   ./subtrees.md
