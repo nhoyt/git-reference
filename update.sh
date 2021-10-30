6ff56d637a5ce23cb23068c0906cfabc955145de
Nicholas Hoyt <nick@webui.com>
2021-10-30 11:46:01 -0500

    Replacing old files

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SRCDIR=$HOME/Documents/Notes/Reference
-
-cp -fp $SRCDIR/Git-Basics.txt     ./basics.md
-cp -fp $SRCDIR/Git-Commands.txt   ./commands.md
-cp -fp $SRCDIR/Git-Submodules.txt ./submodules.md
-cp -fp $SRCDIR/Git-Subtrees.txt   ./subtrees.md
